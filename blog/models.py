a100f6f89abcc00e4561544ca6cc5fab9ea89a19
tomozoushanxia9 <tomozoushanxia9@gmail.com>
2019-12-17 11:49:57 +0900

    summernote blog

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,15 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+    # title = models.CharField('タイトル', max_length=50)
+    # text = models.TextField('テキスト')
+    # # created_at = models.DateField('作成日', auto_now_add=True)
+    # # updated_at = models.DateField('更新日', auto_now=True)
+    # def __str__(self):
+    #     return self.title
+    # class Meta:
+    #     verbose_name = 'ブログ'
+    #     verbose_name_plural = 'ブログ'
--- portfolio2/settings.py
+++ portfolio2/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio2/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
